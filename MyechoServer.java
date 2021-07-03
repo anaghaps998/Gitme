d7bde85b76ea49c038d984f50753c3feafb6e980
anagha <anaghaps998@gmail.com>
2021-07-03 21:07:50 +0530

    Echo sever program added

---
+++ MyechoServer.java
@@ -0,0 +1,22 @@
+import java.net.*;
+import java.io.*;
+class MyechoServer{
+	public static void main( String args[])throws IOException{
+	ServerSocket ss = new ServerSocket(3333);
+	Socket s = ss.accept();
+	DataInputStream din = new DataInputStream(s.getInputStream());
+	DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	String str = "",str1 = "";
+	while(!str.equals("stop")){
+	str = din.readUTF();
+	System.out.println("Client says: "+str);
+	dout.writeUTF(str);
+	dout.flush();
+	}
+	din.close();
+	s.close();
+	ss.close();
+	}
+}
+		
